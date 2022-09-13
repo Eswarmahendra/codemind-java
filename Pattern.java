799549be028fdcffadaa98cf63ec269b35555314
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-09-13 19:45:28 +0530

    import java.util.Scanner; class java { public static void main(String args[]) { int a,b,i,j,k=1; Scanner sc=new Scanner(System.in); a=sc.nextInt(); for(i=1;i<=a;i++) { for(j=1;j<=a;j++) { System.out.print(i+" "); } System.out.println(); } } }

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class java
+{
+public static void main(String args[])
+{
+int a,b,i,j,k=1;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+for(i=1;i<=a;i++)
+{
+for(j=1;j<=a;j++)
+{ 
+System.out.print(i+" ");
+}
+System.out.println();
+}
+}
+}
