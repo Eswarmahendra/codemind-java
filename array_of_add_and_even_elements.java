abd582f33b0237d76be13a8f11e9cb4d221fdd4f
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-09-13 19:54:39 +0530

    import java.util.Scanner; class evfood { public static void main(String args[]) { int a; Scanner sc=new Scanner(System.in); a=sc.nextInt(); int b[]=new int[a]; for(int i=0;i<a;i++) { b[i]=sc.nextInt(); } for(int i=0;i<a;i++) { if(b[i]%2!=0) { System.out.print(b[i]+" "); } } for(int j=0;j<a;j++) { if(b[j]%2==0) { System.out.print(b[j]+" "); } } } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class evfood
+{
+public static void main(String args[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+int b[]=new int[a];
+for(int i=0;i<a;i++)
+{
+b[i]=sc.nextInt();
+}
+for(int i=0;i<a;i++)
+{
+if(b[i]%2!=0)
+{
+System.out.print(b[i]+" ");
+}
+}
+for(int j=0;j<a;j++)
+{
+if(b[j]%2==0)
+{
+System.out.print(b[j]+" ");
+}
+}
+}
+}
