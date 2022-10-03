4186c5bce11d47695109281f8c285c2c91712fc6
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-03 18:24:33 +0530

    import java.util.Scanner; class uglynum1 { public static void main(String args[]) {     int n,rem=1;     Scanner sc=new Scanner(System.in);     n=sc.nextInt();     int x=0;     while(n!=1)     {     if(n%2==0)     {      n=n/2;     rem=rem*n;     }     else if(n%3==0)     {     n=n/3;     rem=rem*n;     }     else if(n%5==0)     {     n=n/5;     rem=rem*n;     }     else     {     System.out.println("Not Ugly Number");     x=1;     break;     }     }     if(x==0)     {     System.out.println("Ugly Number");     } } }

--- Ugly_number.java
+++ Ugly_number.java
@@ -3,37 +3,37 @@ class uglynum1
 {
 public static void main(String args[])
 {
-int n,rem=1;
-Scanner sc=new Scanner(System.in);
-n=sc.nextInt();
-int x=0;
-while(n!=1)
-{
-if(n%2==0)
-{
- n=n/2;
-rem=rem*n;
-}
-else if(n%3==0)
-{
-n=n/3;
-rem=rem*n;
-}
-else if(n%5==0)
-{
-n=n/5;
-rem=rem*n;
-}
-else
-{
-System.out.println("Not Ugly Number");
-x=1;
-break;
-}
-}
-if(x==0)
-{
-System.out.println("Ugly Number");
-}
+    int n,rem=1;
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    int x=0;
+    while(n!=1)
+    {
+    if(n%2==0)
+    {
+     n=n/2;
+    rem=rem*n;
+    }
+    else if(n%3==0)
+    {
+    n=n/3;
+    rem=rem*n;
+    }
+    else if(n%5==0)
+    {
+    n=n/5;
+    rem=rem*n;
+    }
+    else
+    {
+    System.out.println("Not Ugly Number");
+    x=1;
+    break;
+    }
+    }
+    if(x==0)
+    {
+    System.out.println("Ugly Number");
+    }
 }
 }
