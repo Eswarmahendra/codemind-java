2c71c8d1e0c2f750487deceac053f952ab1a8795
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-03 19:55:50 +0530

    import java.util.Scanner; class nearestprime {     public static boolean isprime(int x)     {         int count=0;         for(int i=2;i<=Math.sqrt(x);i++)         {             if(x%i==0)             count++;         }         if(count==0)         return true;         else         return false;     }     public static void main(String args[])     {         int i,t,n,m,d1,d2=0,diff1,diff2;         Scanner sc=new Scanner(System.in);         t=sc.nextInt();         {            for(int j=1;j<=t;j++)            {
            n=sc.nextInt();
    	m=n;
            for(i=n; ;i++)
            {
                if(isprime(i))
    		{
               	
    		d1=i;
    		break;
    		}
    	}
    	for(i=n;i>1 ;i--)
            {
                if(isprime(i))
    		{
                	
    		d2=i;
    		break;
    		}
    	}
    	diff1=Math.abs(m-d1);
    	diff2=m-d2;
    	if(diff1<diff2)
    	System.out.println(d1);
           else
    	System.out.println(d2); 
        }
        }
        }
    }

---
+++ Nearest_Prime.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+class nearestprime
+{
+    public static boolean isprime(int x)
+    {
+        int count=0;
+        for(int i=2;i<=Math.sqrt(x);i++)
+        {
+            if(x%i==0)
+            count++;
+        }
+        if(count==0)
+        return true;
+        else 
+        return false;
+    }
+    public static void main(String args[])
+    {
+        int i,t,n,m,d1,d2=0,diff1,diff2;
+        Scanner sc=new Scanner(System.in);
+        t=sc.nextInt();
+        {
+           for(int j=1;j<=t;j++)
+           {
+        
+        n=sc.nextInt();
+	m=n;
+        for(i=n; ;i++)
+        {
+            if(isprime(i))
+		{
+           	
+		d1=i;
+		break;
+		}
+	}
+	for(i=n;i>1 ;i--)
+        {
+            if(isprime(i))
+		{
+            	
+		d2=i;
+		break;
+		}
+	}
+	diff1=Math.abs(m-d1);
+	diff2=m-d2;
+	if(diff1<diff2)
+	System.out.println(d1);
+       else
+	System.out.println(d2); 
+    }
+    }
+    }
+}
