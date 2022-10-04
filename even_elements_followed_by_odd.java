b727303c6243bf62079a07501f520f39ad752425
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-04 19:30:01 +0530

    import java.util.Scanner; class Array {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(x[i]%2==0)             System.out.print(x[i] +" ");         }         for(int i=0;i<n;i++)         {             if(x[i]%2!=0)             System.out.print(x[i] +" ");         }     }
    }

--- even_elements_followed_by_odd.java
+++ even_elements_followed_by_odd.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
-class evfood
+class Array
 {
-public static void main(String args[])
-{
-int a;
-Scanner sc=new Scanner(System.in);
-a=sc.nextInt();
-int b[]=new int[a];
-for(int i=0;i<a;i++)
-{
-b[i]=sc.nextInt();
-}
-for(int i=0;i<a;i++)
-{
-if(b[i]%2==0)
-{
-System.out.print(b[i]+" ");
-}
-}
-for(int j=0;j<a;j++)
-{
-if(b[j]%2!=0)
-{
-System.out.print(b[j]+" ");
-}
-}
-}
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]%2==0)
+            System.out.print(x[i] +" ");
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]%2!=0)
+            System.out.print(x[i] +" ");
+        }
+    }
+
+    
 }
