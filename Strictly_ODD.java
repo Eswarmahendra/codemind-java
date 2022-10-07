a288d1d5196415dafa79dd258cce8213f9522d04
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-07 13:33:26 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,count1=0,count2=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]%2!=0)             count1++;         }         for(int i=0;i<n;i++)         {             if(x[i]%2!=0 && i%2!=0)             count2++;         }         if(count1==count2)         System.out.print("True");         else         System.out.println("False");     } }

---
+++ Strictly_ODD.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Even
+{
+    public static void main(String args[])
+    {
+        int n,count1=0,count2=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]%2!=0)
+            count1++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]%2!=0 && i%2!=0)
+            count2++;
+        }
+        if(count1==count2)
+        System.out.print("True");
+        else
+        System.out.println("False");
+    }
+}
