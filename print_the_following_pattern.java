d4990dc6c50f5f582b67c8907ccaa88d9d2a48a3
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-13 17:21:38 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int N = sc.nextInt();         for(int i=1; i<=N; i++)         {             if(i > 1 && i < N)             {                 int p = i-2;                 int q = N-i;                 System.out.print("*");                 for(int j=1; j<=p;j++)                     System.out.print(" ");                 System.out.print("*");                 for(int k=1; k<=q; k++)                     System.out.print(" ");             }             else if(i == N)             {                 int j=i;                 while(j > 0)                 {                     System.out.print("*");                     j--;                 }             }             else if(i == 1)             {                 int q = N-i;                 System.out.print("*");                 while(q > 0)                 {                     System.out.print(" ");                     q--;                 }             }             if(i != N)             {                 System.out.print(" ");             }         }         sc.close();     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,48 @@
 import java.util.Scanner;
-class patpra
+class Main
 {
-public static void main(String args[])
-{
-int a,b,i,j,k=1;
-Scanner sc=new Scanner(System.in);
-a=sc.nextInt();
-for(i=a;i>=1;i--)
-{
-for(j=1;j<=i;j++)
-{ 
-System.out.print((char)(64+i)+" ");
-}
-System.out.println();
-}
-}
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++)
+        {
+            if(i > 1 && i < N)
+            {
+                int p = i-2;
+                int q = N-i;
+                System.out.print("*");
+                for(int j=1; j<=p;j++)
+                    System.out.print(" ");
+                System.out.print("*");
+                for(int k=1; k<=q; k++)
+                    System.out.print(" ");
+            }
+            else if(i == N)
+            {
+                int j=i;
+                while(j > 0)
+                {
+                    System.out.print("*");
+                    j--;
+                }
+            }
+            else if(i == 1)
+            {
+                int q = N-i;
+                System.out.print("*");
+                while(q > 0)
+                {
+                    System.out.print(" ");
+                    q--;
+                }
+            }
+            if(i != N)
+            {
+                System.out.print("
+");
+            }
+        }
+        sc.close();
+    }
 }
