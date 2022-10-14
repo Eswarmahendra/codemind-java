538d1e8d2552f4d927856078803cc9d2b05c614c
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-14 17:38:47 +0530

    import java.util.Scanner; class Min {     public static void main(String args[])     {         int n,found=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         int a,b,max=0;         a=sc.nextInt();         b=sc.nextInt();         for(int i=0;i<n;i++)         {             if(x[i]>=a && x[i]<=b)             {                 if(x[i]>max)                 {                     max=x[i];                     found=1;                 }             }         }         if(found==0)         System.out.println("-1");         else         System.out.println(max);     } }

--- maximum_of_numbers_between_A_and_B.java
+++ maximum_of_numbers_between_A_and_B.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
-class Main
+class Min
 {
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int arr[] = new int[N];
-        for(int i=0; i<N; i++)
-            arr[i] = sc.nextInt();
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int max = 0;
-        for(int i=0; i<N; i++)
+        int n,found=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        int a,b,max=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(int i=0;i<n;i++)
         {
-            if(arr[i] >= a && arr[i] <= b)
+            if(x[i]>=a && x[i]<=b)
             {
-                if(max < arr[i])
-                    max = arr[i];
+                if(x[i]>max)
+                {
+                    max=x[i];
+                    found=1;
+                }
             }
         }
-        if(max == 0)
-            System.out.println(-1);
+        if(found==0)
+        System.out.println("-1");
         else
-            System.out.print(max);
-    sc.close();
+        System.out.println(max);
     }
 }
