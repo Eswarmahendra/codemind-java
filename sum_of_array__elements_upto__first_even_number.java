c75b45282784285e59f0128979dd2484acf8643b
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-14 18:07:18 +0530

    import java.util.Scanner; class Min {     public static void main(String args[])     {         int n,found=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         int a,sum=0;         for(int i=0;i<n;i++)         {
                if(x[i]%2==0)
                break;
                else
                sum=sum+x[i];
            }
           System.out.println(sum);
            
        }
    }

---
+++ sum_of_array__elements_upto__first_even_number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Min
+{
+    public static void main(String args[])
+    {
+        int n,found=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        int a,sum=0;
+        for(int i=0;i<n;i++)
+        {
+            
+            if(x[i]%2==0)
+            break;
+            else
+            sum=sum+x[i];
+        }
+       System.out.println(sum);
+        
+    }
+}
