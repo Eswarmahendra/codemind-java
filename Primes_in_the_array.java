d2d37f467c9dd584cb1efa0def04de08eed86f8c
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-14 18:56:02 +0530

    import java.util.Scanner; class PrimesInRange {   public static boolean isPrime(int n)   {      int count=0;      if(n==1)      return false; 	 for(int i=2;i<=(int)Math.sqrt(n);i++) 	 { 	    if(n%i==0) 		  count++; 	 } 	 if(count==0) 	    return true; 	 else 	    return false;   }    public static void main(String args[])   {      Scanner sc=new Scanner(System.in); 	 int n,c=0,sum=0,min=9,max=0; 	 n=sc.nextInt(); 	 int x[]=new int[n]; 	 for(int i=0;i<n;i++) 	 x[i]=sc.nextInt();      for(int i=0;i<n;i++)      {          if(x[i]>max)          max=x[i];      }      for(int i=0;i<n;i++)      {          if(x[i]<min)          min=x[i];      } 	 for(int i=0;i<n;i++) 	 {
    	     if(isPrime(x[i]))
    		 {
    				c++;
    		 }
    	 }
    	 System.out.println(c);
    	 
      }
      
    }

--- Primes_in_the_array.java
+++ Primes_in_the_array.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-class PrimesorNot
+class PrimesInRange
 {
-  public static boolean isprime(int x)
+  public static boolean isPrime(int n)
   {
      int count=0;
-     if(x==1)
+     if(n==1)
      return false;
-	 for(int i=2;i<=(int)Math.sqrt(x);i++)
+	 for(int i=2;i<=(int)Math.sqrt(n);i++)
 	 {
-	    if(x%i==0)
+	    if(n%i==0)
 		  count++;
 	 }
 	 if(count==0)
@@ -19,23 +19,31 @@ class PrimesorNot
    public static void main(String args[])
   {
      Scanner sc=new Scanner(System.in);
-	 int n,k,c=0,sum=0;
-	 float avg=0;
+	 int n,c=0,sum=0,min=9,max=0;
 	 n=sc.nextInt();
-	
 	 int x[]=new int[n];
-	for(int i=0;i<n;i++)
-	{
-	    x[i]=sc.nextInt();
-	}
-	for(int i=0;i<n;i++)
-	{
-	    if(isprime(x[i]))
-	    {
-	       c++;
-	    }
-	}
-	
-	System.out.println(c);	    
+	 for(int i=0;i<n;i++)
+	 x[i]=sc.nextInt();
+     for(int i=0;i<n;i++)
+     {
+         if(x[i]>max)
+         max=x[i];
+     }
+     for(int i=0;i<n;i++)
+     {
+         if(x[i]<min)
+         min=x[i];
+     }
+	 for(int i=0;i<n;i++)
+	 {
+	  
+	     if(isPrime(x[i]))
+		 {
+				c++;
+		 }
+	 }
+	 System.out.println(c);
+	 
   }
+  
 }
