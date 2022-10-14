30a223799cbd3cae5afe3ae31d2ddb998e431ed8
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-14 18:37:27 +0530

    import java.util.Scanner; class PrimesInRange {   public static boolean isPrime(int n)   {      int count=0;      if(n==1)      return false; 	 for(int i=2;i<=(int)Math.sqrt(n);i++) 	 { 	    if(n%i==0) 		  count++; 	 } 	 if(count==0) 	    return true; 	 else 	    return false;   }    public static void main(String args[])   {      Scanner sc=new Scanner(System.in); 	 int n,c=0,sum=0; 	 float avg; 	 n=sc.nextInt(); 	 int x[]=new int[n]; 	 for(int i=0;i<n;i++) 	 x[i]=sc.nextInt(); 	 int k; 	 k=sc.nextInt(); 	 for(int i=0;i<n;i++) 	 {
    	     if(isPrime(x[i]))
    		 {
    				if(x[i]>=k)
    				c++;
    		 }
    	 }
    	 System.out.println(c);
    	 
      }
      
    }

--- Primes_greater_than_k.java
+++ Primes_greater_than_k.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-class PrimesorNot
+class PrimesInRange
 {
-  public static boolean isprime(int x)
+  public static boolean isPrime(int n)
   {
      int count=0;
-	 for(int i=2;i<=(int)Math.sqrt(x);i++)
+     if(n==1)
+     return false;
+	 for(int i=2;i<=(int)Math.sqrt(n);i++)
 	 {
-	    if(x%i==0)
+	    if(n%i==0)
 		  count++;
 	 }
 	 if(count==0)
@@ -17,25 +19,25 @@ class PrimesorNot
    public static void main(String args[])
   {
      Scanner sc=new Scanner(System.in);
-	 int n,k,c=0,sum=0;
-	 float avg=0;
+	 int n,c=0,sum=0;
+	 float avg;
 	 n=sc.nextInt();
-	
 	 int x[]=new int[n];
-	for(int i=0;i<n;i++)
-	{
-	    x[i]=sc.nextInt();
-	}
+	 for(int i=0;i<n;i++)
+	 x[i]=sc.nextInt();
+	 int k;
 	 k=sc.nextInt();
-	for(int i=0;i<n;i++)
-	{
-	    if(isprime(x[i]))
-	    {
-	       if(x[i]>=k)
-	       c++;
-	    }
-	}
-	
-	System.out.println(c);	    
+	 for(int i=0;i<n;i++)
+	 {
+	  
+	     if(isPrime(x[i]))
+		 {
+				if(x[i]>=k)
+				c++;
+		 }
+	 }
+	 System.out.println(c);
+	 
   }
+  
 }
