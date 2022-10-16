79dcac002544e035b90fd552afa07c652a3fbf82
Eswarmahendra <84895871+Eswarmahendra@users.noreply.github.com>
2022-10-16 09:44:41 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         int c=0;         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String words[]=s1.split(" "); 		System.out.println(words.length);
        }
    }

---
+++ Count_no._of_words.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        int c=0;
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String words[]=s1.split(" ");
+		System.out.println(words.length);
+		
+    }
+}
